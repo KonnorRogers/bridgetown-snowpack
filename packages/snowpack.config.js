ab6ecf458ce1ffb66c20048ebc728476e6a5f14b
paramagicdev <konnor5456@gmail.com>
2020-07-18 09:08:19 -0400

    formatting

--- packages/snowpack.config.js
+++ packages/snowpack.config.js
@@ -7,7 +7,7 @@ const scripts = {
   "mount:frontend": "mount frontend --to /_frontend_",
   "mount:output": "mount output --to /",
   "run:bridgetown": "bundle exec bridgetown build",
-  "run:bridgetown::watch": "$1 --watch"
+  "run:bridgetown::watch": "$1 --watch",
 };
 
 if (
@@ -25,21 +25,18 @@ if (
 module.exports = {
   scripts,
   installOptions: {
-    NODE_ENV: true
+    NODE_ENV: true,
   },
   devOptions: {
     // Default port of bridgetown
     port: 4000,
     open: "none",
     // Default output directory of Bridgetown
-    out: "output"
+    out: "output",
   },
   // Account for users using Docker
   proxy: {
-    "0.0.0.0:4000": "0.0.0.0:4000"
+    "0.0.0.0:4000": "0.0.0.0:4000",
   },
-  plugins: [
-    "@snowpack/plugin-dotenv",
-    "@snowpack/plugin-babel"
-  ],
+  plugins: ["@snowpack/plugin-dotenv", "@snowpack/plugin-babel"],
 };
