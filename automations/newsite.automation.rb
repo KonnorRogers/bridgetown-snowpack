f436df91059d1eee3bbaaed940ee789ad284279f
ParamagicDev <konnor5456@gmail.com>
2020-08-18 12:20:32 -0400

    add newsite automation

--- automations/newsite.automation.rb
+++ automations/newsite.automation.rb
@@ -1,10 +1,13 @@
+require "fileutils"
+
 def pull(name, url)
   say "Adding #{name}", :blue
   run "bundle exec bridgetown apply #{url}"
 end
 
 automations = {
-  Tailwind: "",
+  Snowpack: "https://github.com/ParamagicDev/bridgetown-snowpack/blob/master/automations/snowpack.automation.rb",
+  Tailwind: "https://github.com/ParamagicDev/bridgetown-snowpack/blob/master/automations/tailwind.automation.rb",
   Stimulus: "https://github.com/ParamagicDev/bridgetown-automation-stimulus",
   Turbolinks: "https://github.com/ParamagicDev/bridgetown-automation-turbolinks",
   Docker: "https://github.com/ParamagicDev/bridgetown-automation-docker-compose",
@@ -14,3 +17,9 @@ automations = {
 automations.each do |name, url|
   pull(name.to_s, url)
 end
+
+def remove_index_scss
+  FileUtils.rm_rf(File.join("frontend", "styles", "index.scss"))
+end
+
+remove_index_scss
